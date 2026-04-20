8c15029681b13d2c7628a62ccbca37f9ba7defc1
Ariel54698 <arie.chn1@gmail.com>
2026-04-20 14:12:46 +0300

    fix cloud run config

--- app.py
+++ app.py
@@ -30,6 +30,5 @@ def status():
     })
 
 if __name__ == "__main__":
-    flask_host = os.getenv("FLASK_HOST", "127.0.0.1")
-    flask_port = int(os.getenv("FLASK_PORT", "5000"))
-    app.run(host=flask_host, port=flask_port)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
